c3bbdf0c2e294fb52a24fb16d405385bf15fa634
Lucas Henrique Jurgensen <lucas_jurgensen@hotmail.com>
2024-10-31 22:54:29 -0300

    Update Pigeonhole sort 0.5
    We added a fuction to create the Pigeonhole Sort using the dinamic memory allocation.

--- Pigeonhole-sort.c
+++ Pigeonhole-sort.c
@@ -3,9 +3,10 @@
 
 typedef struct arrayInicial
 {
-    int *p;
+    int *p, *q;
     int tamanhoArray;
     int Vmax, Vmin;
+    int sizePgh;
 }arrayInicial;
 
 
@@ -26,6 +27,21 @@ do{
     return arr;
 }
 
+//varredura
+
+
+arrayInicial createPgh (arrayInicial arr){
+    arr.sizePgh = arr.Vmax - arr.Vmin + 1;
+
+    arr.q =  (int*) malloc(arr.sizePgh * sizeof(int));
+
+    for (int i=0; i < arr.sizePgh; i++){
+        arr.q[i] = 1;
+    }
+    return arr;
+}
+
+
 int main(){
     arrayInicial arr;
     arr.p = NULL;
@@ -33,8 +49,10 @@ int main(){
     arr = insercao(arr);
 
     arr.Vmax = arr.p[0];
-    arr.Vmin = arr.p[0];
-    arr = varredura(arr);
+    arr.Vmin = arr.p[1];
+    //arr = varredura(arr);
+
+    arr = createPgh(arr);
 
     printf("Array = { ");
     for(int t=0; t< arr.tamanhoArray; t++){
@@ -42,4 +60,10 @@ int main(){
     }
     printf("} \n");
     printf("Tamanho do Array: %d", arr.tamanhoArray);
+    
+    printf("\n Pigeonhole = { ");
+    for(int t=0; t<arr.sizePgh; t++){
+        printf("%d,", arr.q[t]);
+    }
+    printf("} \n");
 }
